d0b543ef4e6992cdde07f2309ab3dd00b4b901a9
khushbu <khushbusoni475@gmail.com>
2023-06-25 16:13:52 +0530

    [khushbu] Adding array of Integer, Double and Character

---
+++ GenericsDemo/PrintArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    internal class PrintArray
+    {
+        public static void ToPrint(int[] intArray)
+        {
+            Console.WriteLine("\nInteger array Elements");
+            foreach(int elements in intArray)
+            {
+                Console.Write(elements + " ");
+            }
+          
+        }
+
+        public static void ToPrint(double[] doubleArray)
+        {
+            Console.WriteLine("\nDouble array Elements");
+            foreach (double elements in doubleArray)
+            {
+                Console.Write(elements + " ");
+            }
+
+        }
+
+        public static void ToPrint(char[] charArray)
+        {
+            Console.WriteLine("\nString array Elements");
+            foreach (char elements in charArray)
+            {
+                Console.Write(elements + " ");
+            }
+        }
+
+       
+
+    }
+
+}
---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please select any program");
+            Console.WriteLine("1.Without generic method\n2.With generic method");
+            int option = Convert.ToInt32(Console.ReadLine());
+            int[] arr = { 20, 30, 40 };
+            double[] doublearr = { 20.67, 30, 40.34 };
+            char[] chararr = { 'a', 'b', 'c', };
+            switch (option)
+            {
+                case 1:
+                    PrintArray.ToPrint(arr);
+                    PrintArray.ToPrint(doublearr);
+                    PrintArray.ToPrint(chararr);
+                    Console.ReadLine();
+                    break;
+               
+                default:
+                    {
+                        Console.WriteLine("please choose given number");
+                        break;
+                    }
+                   
+            }
+           
+
+           
+        }
+    }
+}
