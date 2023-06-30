cc46f95817ab3cf07c732cca03dec853a1d4b4a2
khushbu <khushbusoni475@gmail.com>
2023-06-30 12:54:01 +0530

    modify find maximum integer wwith the use of generics

---
+++ GenericsDemo/FindMaxValue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    internal class FindMaxValue
+    {
+        public static void FindMaximumValue(int first, int second, int third)
+        {
+            int res = first.CompareTo(second);
+            if(first.CompareTo(second) >= 0 && first.CompareTo(third)>=0)
+            {
+                Console.WriteLine("{0} is greater", first);
+            }
+            else if(second.CompareTo(first)>=0 && second.CompareTo(third)>=0)
+            {
+                Console.WriteLine("{0} is greater", second);
+            }
+            else
+            {
+                Console.WriteLine("{0} is greater", third);
+            }
+        }
+    }
+}
--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -12,11 +12,12 @@ namespace GenericsDemo
         static void Main(string[] args)
         {
             Console.WriteLine("please select any program");
-            Console.WriteLine("1.Without generic method\n2.With generic method\n3.With generic class");
+            Console.WriteLine("1.Without generic method\n2.With generic method\n3.With generic class\n4.Find maximum value");
             int option = Convert.ToInt32(Console.ReadLine());
             int[] arr = { 20, 30, 40 };
             double[] doublearr = { 20.67, 30, 40.34 };
             char[] chararr = { 'a', 'b', 'c', };
+            int first = 30, second = 50, third = 60;
             switch (option)
             {
                 case 1:
@@ -38,6 +39,11 @@ namespace GenericsDemo
                     obj2.ToPrint();
                     PrintGenericClass<char> obj3 = new PrintGenericClass<char>(chararr);
                     obj3.ToPrint();
+                    Console.ReadLine();
+                    break;
+                case 4:
+                    Console.WriteLine("{0} {1} {2}", first, second, third);
+                    FindMaxValue.FindMaximumValue(first, second, third);
                     Console.ReadLine();
                     break;
                 default:
